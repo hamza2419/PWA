56c176577958badc5b1969c9d78280c560dde17c
Hamza <hamza_1904@outlook.com>
2024-11-26 15:37:22 +0400

    updates

---
+++ Products.js
@@ -0,0 +1,51 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": " a 25 pound bag of <em> irresistible</em>, organic goodness for your pet",
+        "price": 20.00,
+        "image": "cat.jpg",
+        "availableInventory": 10,
+        "rating": 2
+
+    },
+    {"id": 1002,
+        "title": "Yarn",
+        "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
+        "price": 2.99,
+        "image": "cat.jpg",
+        "availableInventory": 7,
+        "rating": 3
+},
+{
+    "id": 1003,
+        "title": "Cat tower",
+        "description": "A home and playground for your cat in one!",
+        "price": 15.00,
+        "image": "cat.jpg",
+        "availableInventory": 8,
+        "rating": 4
+
+},
+{
+    "id": 1004,
+        "title": "Laser pointer",
+        "description": "A fun way to keep your cat distracted!",
+        "price": 1.99,
+        "image": "cat.jpg",
+        "availableInventory": 9,
+        "rating": 4
+
+},
+{
+    "id": 1005,
+        "title": "Litter box",
+        "description": "A place for your cat litter",
+        "price": 12.00,
+        "image": "cat.jpg",
+        "availableInventory": 7,
+        "rating": 3
+
+}
+
+]
